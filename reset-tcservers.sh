9b498270b5da60e976075e272e862bb70887630f
nz4ypt <dong.go@pg.com>
2020-08-08 21:23:27 -0400

    update reset scripts

--- reset-tcservers.sh
+++ reset-tcservers.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 #docker network create tcnetwork
 
@@ -7,29 +8,25 @@ docker image rm tc-web-pool-12
 
 docker build --force-rm=true --no-cache=true -t tc-web-pool-12 -f Dockerfile-webpool . 
 
-docker run -tid \
-    -p 8080:8080 -p 8081:8081 \
-    --name docker-tcserver1 \
-	--add-host docker-host:192.168.1.15 \
-	--hostname docker-tcserver1 \
-	--network tcnetwork \
-    -v /opt/dockersrc/siemens/tc12.2.0.4:/apps/siemens/tc12.2.0.4 \
-    -v /opt/dockersrc/siemens/tcdata:/data/tcdata \
-    -v /opt/dockersrc/siemens/tclogs:/data/tclogs \
-    -v /opt/dockersrc/siemens/tcapps:/apps/siemens/tcapps \
-    -v /opt/dockersrc/siemens/scripts:/apps/scripts \
-	tc-web-pool-12
-	
-docker run -tid \
-    -p 8090:8080 -p 8091:8081 \
-    --name docker-tcserver2 \
-	--add-host docker-host:192.168.1.15 \
-	--hostname docker-tcserver2 \
-	--network tcnetwork \
-    -v /opt/dockersrc/siemens/tc12.2.0.4:/apps/siemens/tc12.2.0.4 \
-    -v /opt/dockersrc/siemens/tcdata:/data/tcdata \
-    -v /opt/dockersrc/siemens/tclogs:/data/tclogs \
-    -v /opt/dockersrc/siemens/tcapps:/apps/siemens/tcapps \
-    -v /opt/dockersrc/siemens/scripts:/apps/scripts \
-	tc-web-pool-12
+# publish 8080 is optional if behind load balancer
+# 8081 (jmx) optional
+
+HOST_IP=192.168.1.15
+# number of TCServer
+SRV=2
+
+for i in $(seq $SRV); do 
+    docker run -tid \
+        -p 8080:8080 -p 8081:8081 \
+        --name docker-tcserver$i \
+        --add-host docker-host:$HOST_IP \
+        --hostname docker-tcserver$i \
+        --network tcnetwork \
+        -v /opt/dockersrc/siemens/tc12.2.0.4:/apps/siemens/tc12.2.0.4 \
+        -v /opt/dockersrc/siemens/tcdata:/data/tcdata \
+        -v /opt/dockersrc/siemens/tclogs:/data/tclogs \
+        -v /opt/dockersrc/siemens/tcapps:/apps/siemens/tcapps \
+        -v /opt/dockersrc/siemens/scripts:/apps/scripts \
+        tc-web-pool-12
+done
 
