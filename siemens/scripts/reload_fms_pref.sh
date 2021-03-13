63784058d3243fa3ec541f649378359b0174c5e0
nz4ypt <dong.gh@pg.com>
2021-03-13 09:19:05 -0500

    initial commit for 12.4 with microservices

--- siemens/scripts/reload_fms_pref.sh
+++ siemens/scripts/reload_fms_pref.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source tcProfile.sh
+#source tcProfile.sh
+source tcprofile
 
 preferences_manager -u=infodba -p=infodba -g=dba -mode=import -scope=SITE -file=./prefs.xml -action=OVERRIDE
 
